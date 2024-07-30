1d6e04880f5763dabb3e106cc42d59a4c744bbee
Maria Temponera <marakitempo@yahoo.gr>
2024-07-30 20:28:13 +0300

    Display Tasks

--- script.js
+++ script.js
@@ -21,18 +21,51 @@ document.addEventListener("DOMContentLoaded", function() {
       addTask();
     }
   });
-  deleteButton.addEventListener("click", deleteAllTasks());
+  deleteButton.addEventListener("click", deleteAllTasks);
   displayTasks();
 });
 
 function addTask() {
-
+  const newTask = todoInput.value.trim();
+  if (newTask !== "") {
+    todo.push({
+      text: newTask,
+      disabled: false,
+    });
+    saveToLocalStorage();
+    todoInput.value = "";
+    displayTasks();
+  }
 }
 
 function deleteAllTasks() {
-
+  console.log("test");
 }
 
 function displayTasks() {
-  
+  todoList.innerHTML = "";
+  todo.forEach((item, index) => {
+    const p = document.createElement("p");
+    p.innerHTML = `
+      <div class="todo-container">
+      <input type="checkbox" class="todo-checkbox" id="input-${index}" ${item.disabled ? "checked" : ""}>
+      <p id="todo-${index}" class="${item.disabled ? "disabled" : ""
+      }" onclick="editTask(${index})">${item.text}</p>
+      </div>
+    `;
+    p.querySelector(".todo-checkbox").addEventListener ("change", () => {
+      toggleTask(index);
+    });
+    todoList.appendChild(p);
+  });
+}
+
+function toggleTask(index) {
+  todo[index].disabled = !todo[index].disabled;
+  saveToLocalStorage();
+  displayTasks();
+}
+
+function saveToLocalStorage() {
+  localStorage.setItem("todo", JSON.stringify(todo));
 }
